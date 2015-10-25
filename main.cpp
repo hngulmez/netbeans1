aa5ad7b97e3b1e8cced24f0f7b9704d28ef78197
nuri <nuri@nuri-VirtualBox>
2015-10-25 03:50:35 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: nuri
+ *
+ * Created on October 25, 2015, 2:56 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <memory>
+using namespace std;
+
+/*
+ * 
+ */
+
+
+struct S {
+    S (){cout << "ctor";}
+    S (const S& rhs){cout << "copy ctor";}
+    
+    S& operator=(const S& rhs){return *this;}
+
+private:
+    void *ptr = nullptr;
+    string str = "";
+};
+
+
+int main(int argc, char** argv) {
+    
+    S my;
+    S my2(my);
+    
+    return 0;
+}
+
